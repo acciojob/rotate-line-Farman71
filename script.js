d7a91a069e8b918f7ab0f9dda857918f433508db
Farman71 <farmanhussain71@gmail.com>
2025-03-20 12:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-//your JS code here. If required.
- let angle = 0;
-        const line = document.getElementById('line');
+document.addEventListener("DOMContentLoaded", () => {
+    let angle = 0;
+    const line = document.getElementById('line');
+
+    if (line) {
         setInterval(() => {
             angle += 2;
             line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
         }, 20);
+    } else {
+        console.error("Element #line not found!");
+    }
+});
